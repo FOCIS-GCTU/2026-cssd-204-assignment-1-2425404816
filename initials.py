74732116ea8b14b4fca5509d2b6185a60c359eb4
2425404816 <2425404816@live.gctu.edu.gh>
2026-05-31 11:33:35 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,49 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Andrew Laryea Annan
+# STUDENT ID: 2425404816
+# Email: 2425404816@live.gctu.edu.gh
+# Grader: Emma
+#
+# On my honor, Andrew Laryea Annan, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print small initials line with three periods before and after
+    print()
+    print("...ALA...")
+    print()
 
+    # Row 1
+    print("************....AA..........****L...........****....AA........")
+    # Row 2
+    print("************...A..A........****L...........****...A..A........")
+    # Row 3
+    print("************..A....A.......****L...........****..A....A.......")
+    # Row 4
+    print("************.A......A......****L...........****.A......A......")
+    # Row 5
+    print("************A........A.....****L...........****A........A.....")
+    # Row 6
+    print("************AAAAAAAAAA.....****L...........****AAAAAAAAAA.....")
+    # Row 7
+    print("************A........A.....****L...........****A........A.....")
+    # Row 8
+    print("************A........A.....****L...........****A........A.....")
+    # Row 9
+    print("************A........A.....****L...........****A........A.....")
+    # Row 10
+    print("************A........A.....****LLLLLLLLLL...****A........A.....")
 
+    print()
 
-main()
+# Two blank lines before and after main
+#
+
+if __name__ == "__main__":
+    main()
+
+#
